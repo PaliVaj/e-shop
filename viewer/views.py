0f7c285de5c8b25e43d1cb2408bf17a1928edc87
[matoseko] <[matoseko@centrum.sk]>
2021-06-16 10:32:52 +0200

    new views

--- viewer/views.py
+++ viewer/views.py
@@ -1,7 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from viewer.models import  Product
 
@@ -9,3 +10,8 @@ from viewer.models import  Product
 class ProductView(ListView):
     template_name = 'products.html'
     model = Product
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'productsdetail.html'
+    #queryset = Product.objects.all()
